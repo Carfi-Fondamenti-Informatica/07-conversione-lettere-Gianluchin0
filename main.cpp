50a1de28eb3898348f04d42365517b03163eee89
Gianluchin0 <118599894+Gianluchin0@users.noreply.github.com>
2022-12-07 11:45:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,168 +1,169 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
+
 int main(){
-char y;
-cin>>y;
-if(riconoscere(y) == 0){
-    if (y=='a'){
-        cout<<"A";
+    char x;
+    cin >> x;
+    if(funzione(x) == 0){
+        if (x=='a'){
+            cout<<"A";
+        }
+        if (x=='b'){
+            cout<<"B";
+        }
+        if (x=='c'){
+            cout<<"C";
+        }
+        if (x=='d'){
+            cout<<"D";
+        }
+        if (x=='e'){
+            cout<<"E";
+        }
+        if (x=='f'){
+            cout<<"F";
+        }
+        if (x=='g'){
+            cout<<"G";
+        }
+        if (x=='h'){
+            cout<<"H";
+        }
+        if (x=='i'){
+            cout<<"I";
+        }
+        if (x=='j'){
+            cout<<"J";
+        }
+        if (x=='k'){
+            cout<<"K";
+        }
+        if (x=='l'){
+            cout<<"L";
+        }
+        if (x=='m'){
+            cout<<"M";
+        }
+        if (x=='n'){
+            cout<<"N";
+        }
+        if (x=='o'){
+            cout<<"O";
+        }
+        if (x=='p'){
+            cout<<"P";
+        }
+        if (x=='q'){
+            cout<<"Q";
+        }
+        if (x=='r'){
+            cout<<"R";
+        }
+        if (x=='s'){
+            cout<<"S";
+        }
+        if (x=='t'){
+            cout<<"T";
+        }
+        if (x=='u'){
+            cout<<"U";
+        }
+        if (x=='v'){
+            cout<<"V";
+        }
+        if (x=='w'){
+            cout<<"W";
+        }
+        if (x=='x'){
+            cout<<"X";
+        }
+        if (x=='y'){
+            cout<<"Y";
+        }
+        if (x=='z'){
+            cout<<"Z";
+        }
+        if (x=='A'){
+            cout << "a";
+        }
+        if (x=='B'){
+            cout << "b";
+        }
+        if (x=='C'){
+            cout << "c";
+        }
+        if (x=='D'){
+            cout << "d";
+        }
+        if (x=='E'){
+            cout << "e";
+        }
+        if (x=='F'){
+            cout << "f";
+        }
+        if (x=='G'){
+            cout << "g";
+        }
+        if (x=='H'){
+            cout << "h";
+        }
+        if (x=='I'){
+            cout << "i";
+        }
+        if (x=='K'){
+            cout << "k";
+        }
+        if (x=='J'){
+            cout << "j";
+        }
+        if (x=='L'){
+            cout << "l";
+        }
+        if (x=='M'){
+            cout << "m";
+        }
+        if (x=='N'){
+            cout << "n";
+        }
+        if (x=='O'){
+            cout << "o";
+        }
+        if (x=='P'){
+            cout << "p";
+        }
+        if (x=='Q'){
+            cout << "q";
+        }
+        if (x=='R'){
+            cout << "r";
+        }
+        if (x=='S'){
+            cout << "s";
+        }
+        if (x=='T'){
+            cout << "t";
+        }
+        if (x=='U'){
+            cout << "u";
+        }
+        if (x=='V'){
+            cout << "v";
+        }
+        if (x=='W'){
+            cout << "w";
+        }
+        if (x=='X'){
+            cout << "x";
+        }
+        if (x=='Y'){
+            cout << "y";
+        }
+        if (x=='Z'){
+            cout << "z";
+        }
     }
-    if (y=='b'){
-        cout<<"B";
+    else {
+        cout<<"errore";
     }
-    if (y=='c'){
-        cout<<"C";
-    }
-    if (y=='d'){
-        cout<<"D";
-    }
-    if (y=='e'){
-        cout<<"E";
-    }
-    if (y=='f'){
-        cout<<"F";
-    }
-    if (y=='g'){
-        cout<<"G";
-    }
-    if (y=='h'){
-        cout<<"H";
-    }
-    if (y=='i'){
-        cout<<"I";
-    }
-    if (y=='j'){
-        cout<<"J";
-    }
-    if (y=='k'){
-        cout<<"K";
-    }
-    if (y=='l'){
-        cout<<"L";
-    }
-    if (y=='m'){
-        cout<<"M";
-    }
-    if (y=='n'){
-        cout<<"N";
-    }
-    if (y=='o'){
-        cout<<"O";
-    }
-    if (y=='p'){
-        cout<<"P";
-    }
-    if (y=='q'){
-        cout<<"Q";
-    }
-    if (y=='r'){
-        cout<<"R";
-    }
-    if (y=='s'){
-        cout<<"S";
-    }
-    if (y=='t'){
-        cout<<"T";
-    }
-    if (y=='u'){
-        cout<<"U";
-    }
-    if (y=='v'){
-        cout<<"V";
-    }
-    if (y=='w'){
-        cout<<"W";
-    }
-    if (y=='x'){
-        cout<<"X";
-    }
-    if (y=='y'){
-        cout<<"Y";
-    }
-    if (y=='z'){
-        cout<<"Z";
-    }
-    if (y=='A') {
-        cout << "a";
-    }
-    if (y=='B') {
-        cout << "b";
-    }
-    if (y=='C') {
-        cout << "c";
-    }
-    if (y=='D') {
-        cout << "d";
 }
-    if (y=='E') {
-        cout << "e";
-    }
-    if (y=='F') {
-        cout << "f";
-    }
-    if (y=='G') {
-        cout << "g";
-    }
-    if (y=='H') {
-        cout << "h";
-    }
-    if (y=='I') {
-        cout << "i";
-    }
-    if (y=='K') {
-        cout << "k";
-    }
-    if (y=='J') {
-        cout << "j";
-    }
-    if (y=='L') {
-        cout << "l";
-    }
-    if (y=='M') {
-        cout << "m";
-    }
-    if (y=='N') {
-        cout << "n";
-    }
-    if (y=='O') {
-        cout << "o";
-    }
-    if (y=='P') {
-        cout << "p";
-    }
-    if (y=='Q') {
-        cout << "q";
-    }
-    if (y=='R') {
-        cout << "r";
-    }
-    if (y=='S') {
-        cout << "s";
-    }
-    if (y=='T') {
-        cout << "t";
-    }
-    if (y=='U') {
-        cout << "u";
-    }
-    if (y=='V') {
-        cout << "v";
-    }
-    if (y=='W') {
-        cout << "w";
-    }
-    if (y=='X') {
-        cout << "x";
-    }
-    if (y=='Y') {
-        cout << "y";
-    }
-    if (y=='Z') {
-        cout << "z";
-    }
-}
-else {
-    cout<<"errore";
-}
-    }
